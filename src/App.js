5c41bc7344c0bd80f7e1c59ec87b7597339ad340
Sam Ward-Pickering <samuel.wardpickering@gmail.com>
2022-04-08 17:20:26 +1200

    Fix code/revert

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import MovieList from './components/MovieList';
-
 
 function App() {
 
@@ -34,62 +33,30 @@ function App() {
         "Type": "movie",
         "Poster": "https://m.media-amazon.com/images/M/MV5BZDIzNzM5MDUtZmI5MC00NGQ5LWFlNzEtYzE3ODIxNDI3ZmNhXkEyXkFqcGdeQXVyNjQ4ODE4MzQ@._V1_SX300.jpg"
     },
-    {
-        "Title": "The Young Indiana Jones Chronicles",
-        "Year": "1992–1993",
-        "imdbID": "tt0103586",
-        "Type": "series",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BY2Q2NTMwMzItNTM2My00ZTgzLTljYmQtOWEwNzVlZjZlYjc1XkEyXkFqcGdeQXVyMjM4MzQ4OTQ@._V1_SX300.jpg"
-    },
-    {
-        "Title": "Lego Indiana Jones: The Original Adventures",
-        "Year": "2008",
-        "imdbID": "tt1149318",
-        "Type": "game",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BMThkYjA2NDUtYjAyNS00ZDNkLThhYTgtMmFhNzIxZWFiYjYyXkEyXkFqcGdeQXVyOTA1ODU0Mzc@._V1_SX300.jpg"
-    },
-    {
-        "Title": "Indiana Jones and the Fate of Atlantis",
-        "Year": "1992",
-        "imdbID": "tt0245165",
-        "Type": "game",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BNDhmNTZjZDQtM2Y4OS00NWI3LWIzMGEtMTYxNGEzY2UwOWUzXkEyXkFqcGdeQXVyMzMxNDQ0NQ@@._V1_SX300.jpg"
-    },
-    {
-        "Title": "Indiana Jones and the Temple of the Forbidden Eye Ride",
-        "Year": "1995",
-        "imdbID": "tt0764648",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BMzk5ZmEyMTgtYzQ0ZC00YTEwLWExOTUtMDZhZmY5NDQ0OGJlXkEyXkFqcGdeQXVyNzAyNzI4Njc@._V1_SX300.jpg"
-    },
-    {
-        "Title": "The Adventures of Young Indiana Jones: Treasure of the Peacock's Eye",
-        "Year": "1995",
-        "imdbID": "tt0115031",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BOTg2MTc2NDAzOF5BMl5BanBnXkFtZTcwODExNDIyMQ@@._V1_SX300.jpg"
-    },
-    {
-        "Title": "The Adventures of Young Indiana Jones: Travels with Father",
-        "Year": "1996",
-        "imdbID": "tt0154003",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BMjU0NTY3M2UtZGJlYi00MDk4LWE0ZTAtZWI1MzcyZDg3Y2U5XkEyXkFqcGdeQXVyMzU0NzkwMDg@._V1_SX300.jpg"
-    }
   ])
 
+  const getFilms = async () => {
+      const url = "http://www.omdbapi.com/?s=indiana-jones&apikey=fd06dd26"
+
+      const response = await fetch(url)
+
+      const responseJson = await response.json();
+
+      console.log(responseJson)
+  }
+
+  useEffect(() => {
+    getFilms()
+  }, [])
   
-    
-
-
   
   
 
   return (
-    <div>
-      <MovieList films={films} />
-
-    </div>
+                
+    <>
+        <MovieList films={films} />
+    </>
   );
 }
 
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import {Card} from 'react-bootstrap'
 
 function MovieList (props) {
   return (
     <>
         {props.films.map((film, index) => (
-            <Card style={{ width: '20rem' }} key={film.imdbID}>
+            <div key={film.imdbID}>
                 <img src={film.Poster} alt="movie poster"/>
-                <p>{film.Title}</p>
-            </Card>
+            </div>
         ))}
     </>
   )
